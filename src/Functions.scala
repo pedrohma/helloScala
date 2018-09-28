e414a5c91c2e756d28cf69c5b3f6428b355611b3
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-09-28 11:29:22 -0700

    simple math functions

---
+++ src/Functions.scala
@@ -0,0 +1,21 @@
+object Functions {
+  def multiply(x: Int, y: Int) : Unit = {
+    println(s"Multiplying: ${x} * ${y} = ${x * y}")
+  }
+
+  def add1(x: Int): Unit = {
+    println(s"Adding: ${x} + 1 = ${x + 1}")
+  }
+
+  def subtract1(x: Int): Unit = {
+    println(s"Subtract: ${x} - 1 = ${x - 1}")
+  }
+
+  def square(x: Int) : Unit = {
+    println(s"Subtract: ${x} * ${x} = ${x * x}")
+  }
+}
+
+
+
+
--- src/Hello.scala
+++ src/Hello.scala
@@ -1,3 +1,10 @@
 object Hello extends App {
   println("Hello Scala!")
+
+  Functions.add1(500)
+  Functions.subtract1(500)
+  Functions.multiply(4,5)
+  Functions.square(4)
+
 }
+
